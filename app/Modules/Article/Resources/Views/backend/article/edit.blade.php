4c9d5f99dddd38b726aec99ba6cc507bb51e9e55
baz <r.c67@hotmail.com>
2016-09-19 22:46:52 +0300

    revisionable eklendi

--- app/Modules/Article/Resources/Views/backend/article/edit.blade.php
+++ app/Modules/Article/Resources/Views/backend/article/edit.blade.php
@@ -4,7 +4,7 @@
 {{--@section('title-description'){{trans('common.edit')}}@stop--}}
 
 @section('content')
-    @include('article::backend.article._form', ['record' => $record])
+    @include('article::backend.article._form', ['record' => $record, 'histories' => $histories])
 @stop
 
 
