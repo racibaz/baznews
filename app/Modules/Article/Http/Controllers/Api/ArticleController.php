1f28f4a5e9d1e1a4784db8cd1a1db057dccc3f44
Recai Cansız <recai.cansiz@hudayivakfi.org>
2016-10-17 15:15:40 +0300

    *article api duzenleme

--- app/Modules/Article/Http/Controllers/Api/ArticleController.php
+++ app/Modules/Article/Http/Controllers/Api/ArticleController.php
@@ -6,9 +6,12 @@ use App\Modules\Article\Models\Article;
 use App\Modules\Article\Models\ArticleCategory;
 use App\Modules\Article\Transformers\ArticleCategoryTransformer;
 use App\Modules\Article\Transformers\ArticleTransformer;
+use Carbon\Carbon;
 use Dingo\Api\Routing\Helpers;
 use Illuminate\Routing\Controller;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\DB;
+use App\Modules\Article\Repositories\ArticleRepository as Repo;
 
 
 class ArticleController extends Controller
@@ -16,13 +19,84 @@ class ArticleController extends Controller
     use Helpers;
 
 
-    public function getArticles()
+
+
+    public function index()
     {
-        $records = Article::all();
-        
+        $expiresAt = Carbon::now()->addMinutes(10);
+
+        $records = Cache::remember('users', $expiresAt, function() {
+
+            return   $this->repo->orderBy('updated_at', 'desc')->findAll();
+
+        });
+
+        return $this->response->collection($records, new ArticleTransformer() )->setStatusCode(200);
+
+    }
+
+
+    public function getArticles($count = null)
+    {
+        //DB::connection()->enableQueryLog();
+
+        $expiresAt = Carbon::now()->addMinutes(10);
+
+//        $records = Cache::remember('art', $expiresAt, function($count) {
+//
+//            if($count != null) {
+//
+//                return  Article::where('is_active',1)->take($count)->orderBy('updated_at', 'desc')->get();
+//
+//            }else {
+//
+//                return  Article::where('is_active',1)->orderBy('updated_at', 'desc')->get();
+//            }
+//
+//        });
+
+
+
+
+
+
+
+        if(Cache::has('articles'))
+        {
+
+            $records = Cache::get('articles');
+        }else
+        {
+            if($count != null) {
+
+                $records =   Article::where('is_active',1)->orderBy('updated_at', 'desc')->take($count)->get();
+
+            }else {
+                $records =  Article::where('is_active',1)->orderBy('updated_at', 'desc')->get();
+            }
+            Cache::store('redis')->put('articles', $records, 10);
+        }
+
+        //$queries  = DB::getQueryLog();
+        //print_r($queries);
         return $this->response->collection($records, new ArticleTransformer() )->setStatusCode(200);
     }
 
+
+
+    public function show($id)
+    {
+        $records = $this->repo->orderBy('updated_at', 'desc')->findAll();
+
+        return $this->response->collection($records, new ArticleTransformer() )->setStatusCode(200);
+    }
+
+    public function getArticleById($id)
+    {
+        $record = $this->repo->find($id);
+
+        return $this->response->item($record, new ArticleTransformer() )->setStatusCode(200);
+    }
 
     public function getArticleCategories()
     {
