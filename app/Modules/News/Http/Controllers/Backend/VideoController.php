4e54fe3cdeaf230e627b32fa285b7d9810472267
unknown <recai.cansiz@recaicansiz.hudayivakfi.org>
2017-02-03 18:36:30 +0300

    ffmpeg

--- app/Modules/News/Http/Controllers/Backend/VideoController.php
+++ app/Modules/News/Http/Controllers/Backend/VideoController.php
@@ -9,6 +9,7 @@ use App\Modules\News\Models\Video;
 use App\Modules\News\Models\VideoGallery;
 use App\Modules\News\Repositories\VideoRepository as Repo;
 use Caffeinated\Themes\Facades\Theme;
+use Pbmedia\LaravelFFMpeg\FFMpeg;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Input;
 use Illuminate\Support\Facades\Redirect;
@@ -85,6 +86,29 @@ class VideoController extends Controller
         foreach ($record->tags as $index => $tag){
             $tagIDs[$index] = $tag->id;
         }
+
+
+
+//        $media = FFMpeg::open(asset('1.pm4'));
+//        $frame = $media->getFrameFromString('00:00:03.37');
+//        dd($frame);
+
+
+
+
+
+//        $ffmpeg = new FFMpeg();
+
+
+
+        FFMpeg::fromDisk('videos')
+            ->open('1.mp4')
+            ->getFrameFromSeconds(10)
+            ->export()
+            ->toDisk(public_path())
+            ->save('FrameAt10sec.png');
+
+
 
         return Theme::view('news::' . $this->getViewName(__FUNCTION__),compact([
             'record',
@@ -164,6 +188,31 @@ class VideoController extends Controller
                     Image::make(public_path('videos/'. $result[1]->id .'/'. $result[1]->thumbnail))
                         ->resize(457, 250)
                         ->save(public_path('videos/'. $result[1]->id .'/257x250_' . $document_name));
+
+
+
+
+
+
+//                    $ffmpeg = FFMpeg::create();
+//                    $video = $ffmpeg->open('video.mpg');
+//                    $video
+//                        ->filters()
+//                        ->resize(new FFMpeg\Coordinate\Dimension(320, 240))
+//                        ->synchronize();
+//                    $video
+//                        ->frame(FFMpeg\Coordinate\TimeCode::fromSeconds(10))
+//                        ->save('frame.jpg');
+//                    $video
+//                        ->save(new FFMpeg\Format\Video\X264(), 'export-x264.mp4')
+//                        ->save(new FFMpeg\Format\Video\WMV(), 'export-wmv.wmv')
+//                        ->save(new FFMpeg\Format\Video\WebM(), 'export-webm.webm');
+
+
+
+
+
+
                 }
 
 
