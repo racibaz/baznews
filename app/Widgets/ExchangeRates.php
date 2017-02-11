26b21401300230d177a10e8dfe49e1c06e46fe7d
recai <r.c67@hotmail.com>
2017-02-11 22:35:26 +0300

    Döviz Kurları Widget eklenmeli

---
+++ app/Widgets/ExchangeRates.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Widgets;
+
+use Arrilot\Widgets\AbstractWidget;
+use Cache;
+use Theme;
+
+class ExchangeRates extends AbstractWidget
+{
+    public function run()
+    {
+        return Cache::remember('exchangeRates', 60, function()  {
+            $connect_web = simplexml_load_file('http://www.tcmb.gov.tr/kurlar/today.xml');
+
+            $usdBuying = $connect_web->Currency[0]->BanknoteBuying;
+            $usdSelling = $connect_web->Currency[0]->BanknoteSelling;
+            $euroBuying = $connect_web->Currency[3]->BanknoteBuying;
+            $euroSelling = $connect_web->Currency[3]->BanknoteSelling;
+
+            return Theme::view('frontend.widgets.exchange_rates',compact([
+                'usdBuying',
+                'usdSelling',
+                'euroBuying',
+                'euroSelling',
+            ]))->render();
+        });
+    }
+}
---
+++ public/themes/news-theme/views/frontend/widgets/exchange_rates.blade.php
@@ -0,0 +1,13 @@
+<div class="widget">
+    <div class="title-section">
+        <h1>
+            <span>{{trans('widget.exchange_rates_title')}}</span>
+        </h1>
+    </div>
+    <ul>
+        <li>Dolar Alış : {{$usdBuying}}</li>
+        <li>Dolar Satış : {{$usdSelling}}</li>
+        <li>Euro Alış : {{$euroBuying}}</li>
+        <li>Euro Alış : {{$euroSelling}}</li>
+    </ul>
+</div>
