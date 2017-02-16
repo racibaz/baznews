c1226613a6155b7102785d306f44bea0ad352298
aliduman <alidumanpr@hotmail.com>
2017-02-16 08:15:01 +0300

    - home video-image gallery slider bug fix..

--- public/themes/news-theme/views/frontend/index.blade.php
+++ public/themes/news-theme/views/frontend/index.blade.php
@@ -343,14 +343,14 @@
                     </div>
                     <div class="th-nw-slide">
                         <div id="m_pg1" class="pager">
-                            @foreach($photoGalleries as $photoGallery)
-                                <a data-slide-index="{{$photoGallery->id}}" href="#" class="bx-pager-link">
+                            @foreach($photoGalleries as $index =>  $photoGallery)
+                                <a data-slide-index="{{$index}}" href="#" class="bx-pager-link">
                                     <span class="img-ct"><img src="{{ asset('gallery/' . $photoGallery->id . '/photos/58x58_' . $photoGallery->thumbnail)}}" /></span>
                                 </a>
                             @endforeach
                         </div><!-- /.m-pg -->
                         <ul class="slide m-slider1" id="photo-gallery-slider">
-                            @foreach($photoGalleries as $photoGallery)
+                            @foreach($photoGalleries as $index =>  $photoGallery)
                                 <li>
                                     <a href="{{route('show_photo_gallery',['slug' => $photoGallery->slug ])}}">
                                         <img src="{{ asset('gallery/' . $photoGallery->id . '/photos/497x358_' . $photoGallery->thumbnail)}}" />
@@ -368,8 +368,8 @@
                     </div>
                     <div class="th-nw-slide">
                         <div id="m_pg2" class="pager">
-                            @foreach($videoGalleries as $videoGallery)
-                                <a data-slide-index="{{$videoGallery->id}}" href="{{route('show_video_gallery',['slug' => $videoGallery->videos->first()->slug ])}}" class="bx-pager-link">
+                            @foreach($videoGalleries as $index =>  $videoGallery)
+                                <a data-slide-index="{{$index}}" href="{{route('show_video_gallery',['slug' => $videoGallery->videos->first()->slug ])}}" class="bx-pager-link">
                                     <span class="img-ct">
                                         <img src="{{ asset('video_gallery/' . $videoGallery->id . '/photos/58x58_' . $videoGallery->thumbnail)}}" />
                                     </span>
@@ -377,7 +377,7 @@
                             @endforeach
                         </div><!-- /.m-pg -->
                         <ul class="slide m-slider1" id="video-gallery-slider">
-                            @foreach($videoGalleries as $videoGallery)
+                            @foreach($videoGalleries as $index =>  $videoGallery)
                                 <li>
                                     <a href="{{route('show_videos',['slug' => $videoGallery->videos->first()->slug ])}}">
                                         <img src="{{ asset('video_gallery/' . $videoGallery->id . '/photos/497x358_' . $videoGallery->thumbnail)}}" />
