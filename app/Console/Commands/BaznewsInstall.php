7c36f766659c612a1f9e21ec218757d79cbfc76d
unknown <recai.cansiz@recaicansiz.hudayivakfi.org>
2017-03-10 15:06:05 +0300

    *Video yönetimi video kategori hatası çözüldü.

--- app/Console/Commands/BaznewsInstall.php
+++ app/Console/Commands/BaznewsInstall.php
@@ -5,14 +5,14 @@ namespace App\Console\Commands;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Redis;
 
-class SystemMigrate extends Command
+class BaznewsInstall extends Command
 {
     /**
      * The name and signature of the console command.
      *
      * @var string
      */
-    protected $signature = 'baznews:migrate';
+    protected $signature = 'baznews:install';
 
     /**
      * The console command description.
