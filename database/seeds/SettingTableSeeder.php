4f514d8dd5457b79fd7e2c7ba7f981f50d07e1cb
HUDAYIVAKFI\recai.cansiz <r.c67@hotmail.com>
2017-03-23 11:27:42 +0300

    *addthis ve disqus kodları geliştirme ortamı için setting seeder a eklendi.

--- database/seeds/SettingTableSeeder.php
+++ database/seeds/SettingTableSeeder.php
@@ -169,7 +169,27 @@ class SettingTableSeeder extends Seeder
 
         Setting::create([
             'attribute_key'               => 'disqus',
-            'attribute_value'             => 'disqus account',
+            'attribute_value'             => '
+            
+<div id="disqus_thread"></div>
+<script>
+var disqus_config = function () {
+this.page.url = \'http://www.baznews.com/{{$record->slug}}\';
+this.page.identifier = \'{{$record->slug}}\';
+this.page.title = \'{{$record->title}}\';
+};
+
+(function() { // DON\'T EDIT BELOW THIS LINE
+var d = document, s = d.createElement(\'script\');
+s.src = \'https://baznews.disqus.com/embed.js\';
+s.setAttribute(\'data-timestamp\', +new Date());
+(d.head || d.body).appendChild(s);
+})();
+</script>
+<noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
+                                                 
+            
+            ',
         ]);
 
         Setting::create([
