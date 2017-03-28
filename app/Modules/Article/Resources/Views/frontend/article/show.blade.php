b502ae3ad4b3f8939138bb92e14b2a749dddb64f
Ali Duman <alidumanpr@hotmail.com>
2017-03-28 17:35:00 +0300

    -makale detay diğer makaleler eklendi

--- app/Modules/Article/Resources/Views/frontend/article/show.blade.php
+++ app/Modules/Article/Resources/Views/frontend/article/show.blade.php
@@ -21,7 +21,7 @@
         </ol>
         <div class="row">
             <div class="col-md-8">
-                <article class="article-content module">
+                <article class="article module">
 
                     <div class="article-head">
                         <div class="author-img">
@@ -43,7 +43,7 @@
                         </div>
                     </div>
 
-                    <div class="article-body-text">
+                    <div class="article-content">
                         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad animi assumenda autem dolor
                             doloremque dolores dolorum enim ipsa maxime minima necessitatibus nemo nesciunt quaerat,
                             quam quidem ratione reiciendis veniam voluptatibus!</p>
@@ -54,6 +54,46 @@
                     {{--yazarı : <a href="{!! route('book_author', ['slug' => $record->book_author->slug]) !!}">{{$record->book_author->name}}</a>--}}
                     {{--yayıncı : <a href="{!! route('book_publisher', ['slug' => $record->book_publisher->slug]) !!}">{{$record->book_publisher->name}}</a>--}}
                 </article>
+
+                <div class="other-article">
+                    <div class="title-section">
+                        <h2>
+                            <span>Diğer Makaleler</span>
+                        </h2>
+                    </div>
+                    <div class="module">
+                        <ul class="article-list">
+                           <li>
+                               <div class="article-title pull-left">
+                                   <a href="#">
+                                       <span>Makale 1</span>
+                                   </a>
+                               </div>
+                               <div class="time pull-right">
+                                   <a href="#">
+                                       <i class="fa fa-clock-o"></i>
+                                       <span>1 Ocak 2017</span>
+                                   </a>
+                               </div>
+                           </li>
+                            <li>
+                                <div class="article-title pull-left">
+                                    <a href="#">
+                                        <span>Makale 1</span>
+                                    </a>
+                                </div>
+                                <div class="time pull-right">
+                                    <a href="#">
+                                        <i class="fa fa-clock-o"></i>
+                                        <span>1 Ocak 2017</span>
+                                    </a>
+                                </div>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+
+
             </div><!-- /.new-content -->
             <div class="col-md-4">
                 <div class="sidebar">
