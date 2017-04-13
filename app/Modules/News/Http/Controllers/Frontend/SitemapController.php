fa19d6eafa25ebf5edc645eae335a5c78a927fda
HUDAYIVAKFI\recai.cansiz <r.c67@hotmail.com>
2017-04-13 17:22:14 +0300

    Sitemaps module lere alındı.

--- app/Modules/News/Http/Controllers/Frontend/SitemapController.php
+++ app/Modules/News/Http/Controllers/Frontend/SitemapController.php
@@ -2,18 +2,18 @@
 
 namespace App\Modules\News\Http\Controllers\Frontend;
 
+use App\Http\Controllers\Controller;
 use App\Modules\News\Models\News;
 use Caffeinated\Themes\Facades\Theme;
-use Illuminate\Http\Request;
-
-use App\Http\Requests;
-use App\Http\Controllers\Controller;
 
 class SitemapController extends Controller
 {
+    /**
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
     public function sitemap()
     {
-        $newsItems = News::where('is_active', 1)->orderBy('updated_at', 'desc')->get();
+        $newsItems = News::where('status', 1)->where('is_active', 1)->orderBy('updated_at', 'desc')->get();
 
         return Theme::response('modules.news.frontend.sitemap.sitemap', compact('newsItems'), 200, [
             'Content-Type' => 'text/xml'
--- database/seeds/SitemapsTableSeeder.php
+++ database/seeds/SitemapsTableSeeder.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Models\Sitemap;
 use Illuminate\Database\Seeder;
 
 class SitemapsTableSeeder extends Seeder
@@ -12,28 +11,6 @@ class SitemapsTableSeeder extends Seeder
      */
     public function run()
     {
-        Sitemap::create([
-            'name'          => 'Kitaplar',
-            'url'           => 'books_sitemap',
-            'last_modified' => \Carbon\Carbon::now(),
-            'order'         => 1,
-            'is_active'     => 1,
-        ]);
-
-        Sitemap::create([
-            'name'          => 'Makaleler',
-            'url'           => 'articles_sitemap',
-            'last_modified' => \Carbon\Carbon::now(),
-            'order'         => 2,
-            'is_active'     => 1,
-        ]);
-
-        Sitemap::create([
-            'name'          => 'Haberler',
-            'url'           => 'news_sitemap',
-            'last_modified' => \Carbon\Carbon::now(),
-            'order'         => 3,
-            'is_active'     => 1,
-        ]);
+        //
     }
 }
