c0d0527efeeb23afea799e6d1e12b29dbb2daf0c
HUDAYIVAKFI\recai.cansiz <r.c67@hotmail.com>
2017-08-02 13:40:43 +0300

    condition added on recommendation widget

--- app/Modules/News/Resources/Views/frontend/widgets/recommendation_news.blade.php
+++ app/Modules/News/Resources/Views/frontend/widgets/recommendation_news.blade.php
@@ -6,27 +6,29 @@
     </div>
     <div>
         <ul class="news-list">
-            @foreach($recommendationNewsItems as $recommendationNewsItem)
-                <li class="module">
-                    <div class="imgwrap">
-                        <a href="{!! route('show_news', ['slug' => $recommendationNewsItem->news->slug]) !!}">
-                            <img src="{{ asset('images/news_images/' . $recommendationNewsItem->news->id . '/58x58_' . $recommendationNewsItem->news->thumbnail )}}"
-                                 alt="{{ $recommendationNewsItem->news->title }}">
-                        </a>
-                    </div><!-- /imgwrap -->
-                    <div class="text">
-                        <div class="title">
+            @if(!empty($recommendationNewsItems))
+                @foreach($recommendationNewsItems as $recommendationNewsItem)
+                    <li class="module">
+                        <div class="imgwrap">
                             <a href="{!! route('show_news', ['slug' => $recommendationNewsItem->news->slug]) !!}">
-                                <h3>{{$recommendationNewsItem->news->title}}</h3>
+                                <img src="{{ asset('images/news_images/' . $recommendationNewsItem->news->id . '/58x58_' . $recommendationNewsItem->news->thumbnail )}}"
+                                     alt="{{ $recommendationNewsItem->news->title }}">
                             </a>
-                        </div><!-- /.title -->
-                        <div class="excerpt">{{$recommendationNewsItem->news->spot}}</div>
-                        <time class="new-date">
-                            <span class="timeago">{{$recommendationNewsItem->news->updated_at->diffForHumans() }}</span>
-                        </time>
-                    </div><!-- /.text -->
-                </li>
-            @endforeach
+                        </div><!-- /imgwrap -->
+                        <div class="text">
+                            <div class="title">
+                                <a href="{!! route('show_news', ['slug' => $recommendationNewsItem->news->slug]) !!}">
+                                    <h3>{{$recommendationNewsItem->news->title}}</h3>
+                                </a>
+                            </div><!-- /.title -->
+                            <div class="excerpt">{{$recommendationNewsItem->news->spot}}</div>
+                            <time class="new-date">
+                                <span class="timeago">{{$recommendationNewsItem->news->updated_at->diffForHumans() }}</span>
+                            </time>
+                        </div><!-- /.text -->
+                    </li>
+                @endforeach
+            @endif
         </ul>
     </div>
 </div>
