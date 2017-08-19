3e4e5c9ce152efc81beaf3e0ee63e6b019259bf2
HUDAYIVAKFI\recai.cansiz <r.c67@hotmail.com>
2017-08-19 11:25:45 +0300

    MAIL_FROM and APP_NAME field added to config files

--- config/mail.php
+++ config/mail.php
@@ -56,8 +56,8 @@ return [
     */
 
     'from' => [
-        'address' => 'hello@example.com',
-        'name' => 'Example',
+        'address' => env('MAIL_FROM'),
+        'name' => env('APP_NAME'),
     ],
 
     /*
