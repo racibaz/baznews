16818863452cf9cafb6e1ca2384c8f065aaa90f5
Recai <r.c67@hotmail.com>
2017-09-18 15:20:10 +0300

    url edited in adverrisement seeder

--- database/seeds/AdvertisementsTableSeeder.php
+++ database/seeds/AdvertisementsTableSeeder.php
@@ -12,34 +12,35 @@ class AdvertisementsTableSeeder extends Seeder
      */
     public function run()
     {
+        $url = url('/');
         Advertisement::create([
             'name'                  => 'left',
-            'code'                  => '<a href="http://bazsoft.biz" target="_blank"><img src="http://baznews.app/img/advert-images/ads-image1.jpeg" alt="bazsoft.biz" /></a>',
+            'code'                  => '<a href="'. $url .'" target="_blank"><img src="'. $url .'/img/advert-images/ads-image1.jpeg" alt="bazsoft.biz" /></a>',
             'is_active'             => 1,
         ]);
 
         Advertisement::create([
             'name'                  => 'right',
-            'code'                  => '<a href="http://bazsoft.biz" target="_blank"><img src="http://baznews.app/img/advert-images/ads-image1.jpeg" alt="bazsoft.biz" /></a>',
+            'code'                  => '<a href="'. $url .'" target="_blank"><img src="'. $url .'/img/advert-images/ads-image1.jpeg" alt="bazsoft.biz" /></a>',
             'is_active'             => 1,
         ]);
 
 
         Advertisement::create([
             'name'                  => 'header_1',
-            'code'                  => '<a href="http://bazsoft.biz" target="_blank"><img src="http://baznews.app/img/advert-images/728x90.png" alt="bazsoft.biz" width="728" height="90" /></a>',
+            'code'                  => '<a href="'. $url .'" target="_blank"><img src="'. $url .'/img/advert-images/728x90.png" alt="bazsoft.biz" width="728" height="90" /></a>',
             'is_active'             => 1,
         ]);
 
         Advertisement::create([
             'name'                  => 'right_block_1',
-            'code'                  => '<a href="http://bazsoft.biz" target="_blank"><img src="http://baznews.app/img/advert-images/336x280.png" alt="bazsoft.biz" width="360" height="280" /></a>',
+            'code'                  => '<a href="'. $url .'" target="_blank"><img src="'. $url .'/img/advert-images/336x280.png" alt="bazsoft.biz" width="360" height="280" /></a>',
             'is_active'             => 1,
         ]);
 
         Advertisement::create([
             'name'                  => 'center_1',
-            'code'                  => '<a href="http://bazsoft.biz" target="_blank"><img src="http://baznews.app/img/advert-images/728x90.png" alt="bazsoft.biz" width="728" height="90" /></a>',
+            'code'                  => '<a href="'. $url .'" target="_blank"><img src="'. $url .'/img/advert-images/728x90.png" alt="bazsoft.biz" width="728" height="90" /></a>',
             'is_active'             => 1,
         ]);
 
