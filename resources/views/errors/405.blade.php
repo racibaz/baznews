de7952f691889f8ad760707920515e334edb4e25
aliduman <alidumanpr@hotmail.com>
2017-09-30 15:17:13 +0300

    custom error page fixed

--- resources/views/errors/405.blade.php
+++ resources/views/errors/405.blade.php
@@ -1,6 +1,9 @@
 <!DOCTYPE html>
 <html>
 <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>{{$message}}</title>
 
     <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
@@ -8,7 +11,7 @@
 </head>
 <body>
 <div class="content-w3">
-    <h1>405</h1>
+    <h1>404</h1>
     <h2>{{$message}}</h2>
     <p>
         <span> <a href="{{route('index')}}" class="back-button">{{trans('common.homepage')}}</a></span>
