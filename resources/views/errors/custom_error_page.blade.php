9ebaa8728d0bdbeab04ddb82e662a71577e31bbd
Recai <r.c67@hotmail.com>
2017-10-13 20:22:01 +0300

    errors pages is refactored.

--- resources/views/errors/custom_error_page.blade.php
+++ resources/views/errors/custom_error_page.blade.php
@@ -1,21 +1,21 @@
 <!DOCTYPE html>
 <html>
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>{{$message}}</title>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>{{$message}}</title>
 
-    <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
-    <link rel="stylesheet" href="{{ asset('css/error.css') }}">
-</head>
-<body>
-<div class="content-w3">
-    <h1>{{$message}}</h1>
-    <p>
-        <span> <a href="{{route('index')}}" class="back-button">{{trans('common.homepage')}}</a></span>
-        <span> <a href="{{redirect()->back()}}" class="back-button">{{trans('common.redirect_back')}}</a></span>
-    </p>
-</div>
-</body>
+        <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+        <link rel="stylesheet" href="{{ asset('css/error.css') }}">
+    </head>
+    <body>
+        <div class="content-w3">
+            <h2>{{$message}}</h2>
+            <p>
+                <span> <a href="{{route('index')}}" class="back-button">{{trans('common.homepage')}}</a></span>
+                <span> <a href="{{redirect()->back()->getTargetUrl()}}" class="back-button">{{trans('common.redirect_back')}}</a></span>
+            </p>
+        </div>
+    </body>
 </html>
