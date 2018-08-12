8981012424515a6b119bf8bcc160e5b929c7a2d5
recai <r.c67@hotmail.com>
2018-08-12 13:26:02 +0300

    recommandation widget of news module optimized

--- app/Modules/News/Repositories/RecommendationNewsRepository.php
+++ app/Modules/News/Repositories/RecommendationNewsRepository.php
@@ -10,13 +10,19 @@ class RecommendationNewsRepository extends EloquentRepository
 
     protected $model = 'App\Modules\News\Models\RecommendationNews';
 
+    //todo query (with) optmize edilecek.
     public function getCuffRecommendationNewsItems($limit = 25)
     {
-        return $this->with(['news'])
+        return $this
+            ->with(['news' => function($query){
+
+                $query->where('is_active', 1);
+
+            }])
             ->where('is_active', 1)
-            ->where('is_cuff', 1)
-            ->take($limit)
+            ->where('is_cuff',1)
             ->orderBy('order', 'asc')
-            ->get();
+            ->findAll()
+            ->take($limit);
     }
 }
